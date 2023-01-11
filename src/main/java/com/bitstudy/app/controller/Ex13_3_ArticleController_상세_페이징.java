a7150fa54f4647d19686222af1c3cc169562adda
Default Profile <opre666@gmail.com>
2023-01-11 20:16:21 +0900

    #22 - 게시글 상세페이지 페이징 구현

---
+++ src/main/java/com/bitstudy/app/controller/Ex13_3_ArticleController_상세_페이징.java
@@ -0,0 +1,67 @@
+package com.bitstudy.app.controller;
+
+
+import com.bitstudy.app.domain.type.SearchType;
+import com.bitstudy.app.dto.response.ArticleResponse;
+import com.bitstudy.app.dto.response.ArticleWithCommentsResponse;
+import com.bitstudy.app.service.ArticleService;
+import com.bitstudy.app.service.PaginationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/articles12_3")
+public class Ex13_3_ArticleController_상세_페이징 {
+
+    private final ArticleService articleService;
+
+    private final PaginationService paginationService;
+
+
+    /** 게시판 리스트 보여주기 */
+    @GetMapping
+    public String articles(
+            @RequestParam(required = false) SearchType searchType,
+            @RequestParam(required = false) String searchValue,
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+            ModelMap map) {
+
+//        map.addAttribute("articles", articleService.searchArticles(searchType,searchValue,pageable).map(ArticleResponse::from));
+
+
+        Page<ArticleResponse> articles = articleService.searchArticles(searchType,searchValue,pageable).map(ArticleResponse::from);
+
+        List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
+
+        map.addAttribute("articles", articles);
+        map.addAttribute("paginationBarNumbers", barNumbers);
+
+        return "articles/index";
+    }
+
+    /** 게시글 상세 페이지 */
+    @GetMapping("/{articleId}")
+    public String article(@PathVariable Long articleId, ModelMap map) {
+        ArticleWithCommentsResponse article = ArticleWithCommentsResponse.from(articleService.getArticle(articleId));
+
+        map.addAttribute("article", article);
+        map.addAttribute("articleComments", article.articleCommentsResponse());
+        // article.articleCommentsResponse() 해설: 현재 article에 ArticleCommentsResponse 의 정보가 담겨있으니까 article 안에 있는 articleComments를 꺼내면 된다.
+
+/*새로 생성*/
+        map.addAttribute("totalCount", articleService.getArticleCount());
+        return "articles/detail";
+    }
+}
---
+++ src/test/java/com/bitstudy/app/controller/Ex13_4_ArticleControllerTest_상세_페이징.java
@@ -0,0 +1,161 @@
+package com.bitstudy.app.controller;
+
+import com.bitstudy.app.config.SecurityConfig;
+import com.bitstudy.app.dto.ArticleWithCommentsDto;
+import com.bitstudy.app.dto.UserAccountDto;
+import com.bitstudy.app.service.ArticleService;
+import com.bitstudy.app.service.PaginationService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@Import(SecurityConfig.class)
+@WebMvcTest(ArticleController.class) 
+@DisplayName("view 컨트롤러 - 게시글")
+class Ex13_4_ArticleControllerTest_상세_페이징 {
+
+    private final MockMvc mvc;
+    @MockBean private ArticleService articleService;
+
+    @MockBean private PaginationService paginationService;
+
+    public Ex13_4_ArticleControllerTest_상세_페이징(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+
+    /**1) 게시판 (리스트) 페이지*/
+    @Test
+    @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상호출")
+    public void articlesAll() throws Exception {
+        /*  eq 는 equal 이라는 뜻  */
+        given(articleService.searchArticles(eq(null), eq(null), any(Pageable.class))).willReturn(Page.empty());
+
+/* 새거 */
+        given(paginationService.getPaginationBarNumbers(anyInt(), anyInt())).willReturn(List.of(0,1,2,3,4));
+
+        mvc.perform(get("/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML)) 
+                .andExpect(view().name("articles/index"))
+                .andExpect(model().attributeExists("articles"))
+                .andExpect(model().attributeExists("paginationBarNumbers"));
+
+        then(articleService).should().searchArticles(eq(null), eq(null), any(Pageable.class));
+        then(paginationService).should().getPaginationBarNumbers(anyInt(), anyInt());
+    }
+
+    /**2) 게시글 (상세) 페이지*/
+    @Test
+    @DisplayName("[view][GET] 게시글 상세 페이지 - 정상호출")
+    public void articlesOne() throws Exception {
+
+        Long articleId = 1L;
+
+/*새거*/ long totalCount = 1L; /* Long은 null 가능, long은 불가*/
+
+        given(articleService.getArticle(articleId)).willReturn(createArticleWithCommentsDto());
+
+/*새거*/ given(articleService.getArticleCount()).willReturn(totalCount);
+
+        mvc.perform(get("/articles/1")) /** 테스트니까 그냥 1번 글 가져와라 할거임 */
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/detail"))
+                .andExpect(model().attributeExists("article"))
+                .andExpect(model().attributeExists("articleComments"))
+/*새거*/         .andExpect(model().attributeExists("totalCount"));
+
+        then(articleService).should().getArticle(articleId);
+/*새거*/ then(articleService).should().getArticleCount();
+    }
+
+//    /**3) 게시판 검색 전용*/
+//    @Disabled("구현중")
+//    @Test
+//    @DisplayName("[view][GET] 게시글 검색 전용 페이지 - 정상호출")
+//    public void articlesSearch() throws Exception {
+//        mvc.perform(get("/articles/search"))
+//                .andExpect(status().isOk())
+//                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+//                .andExpect(view().name("articles/search"));
+//    }
+//
+//    /**4) 해시태그 검색 전용 페이지*/
+//    @Disabled("구현중")
+//    @Test
+//    @DisplayName("[view][GET] 게시글 해시태그 전용 페이지 - 정상호출")
+//    public void articlesSearchHashtag() throws Exception {
+//        mvc.perform(get("/articles/search-hashtag"))
+//                .andExpect(status().isOk())
+//                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+//                .andExpect(view().name("articles/search-hashtag"));
+//    }
+
+    ///////////////////////////////////////
+    private ArticleWithCommentsDto createArticleWithCommentsDto() {
+        return ArticleWithCommentsDto.of(
+                1L,
+                createUserAccountDto(),
+                Set.of(),
+                "title",
+                "content",
+                "#java",
+                LocalDateTime.now(),
+                "bitstudy",
+                LocalDateTime.now(),
+                "bitstudy"
+        );
+    }
+
+    private UserAccountDto createUserAccountDto() {
+        return UserAccountDto.of(
+                1L,
+                "bitstudy",
+                "password",
+                "bitstudy@email.com",
+                "bitstudy",
+                "memo memmo",
+                LocalDateTime.now(),
+                "bitstudy",
+                LocalDateTime.now(),
+                "bitstudy"
+        );
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
